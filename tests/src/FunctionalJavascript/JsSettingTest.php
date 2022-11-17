45cf70b2879a968a3cf38204bd7535f6e58d5a64
tuutti <tuutti@iki.fi>
2022-11-17 08:45:36 +0200

    Example functional js test

---
+++ tests/src/FunctionalJavascript/JsSettingTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\github_actions_example\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Basic functional JS test.
+ *
+ * @group github_actions_example
+ */
+class JsSettingsTest extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'github_actions_example',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests drupalSettings.
+   */
+  public function testSettings() : void {
+    $this->drupalGet('<front>');
+
+    $settings = $this->getDrupalSettings();
+    $this->assertNotEmpty($settings);
+  }
+
+}
+
